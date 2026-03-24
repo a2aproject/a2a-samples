079778c8707392156a2030b6c15eee6b0ec51d61
sokoliva <sokolaj@google.com>
2026-03-24 13:21:19 +0000

    add Task Status Update = TASK_STATE_COMPLETED

--- samples/python/agents/helloworld/agent_executor.py
+++ samples/python/agents/helloworld/agent_executor.py
@@ -60,7 +60,13 @@ class HelloWorldAgentExecutor(AgentExecutor):
                 task_id=context.task_id,
                 context_id=context.context_id,
                 artifact=new_text_artifact(name='result', text=result),
-                last_chunk=True,
+            )
+        )
+        await event_queue.enqueue_event(
+            TaskStatusUpdateEvent(
+                task_id=context.task_id,
+                context_id=context.context_id,
+                status=TaskStatus(state=TaskState.TASK_STATE_COMPLETED),
             )
         )
 
